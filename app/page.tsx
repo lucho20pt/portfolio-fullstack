608d12b5c098d345b2e1bb8738cd177f02f6eb4d
Daniel Batista <dani_batista20@hotmail.com>
2024-10-07 19:02:04 +0100

    add: Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Hero } from '@/components/hero'
+
 export default function Home() {
   return (
     <div className="text-center">
-      <h1>portfolio</h1>
+      <Hero />
     </div>
   )
 }
---
+++ components/hero.tsx
@@ -0,0 +1,26 @@
+'use client'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const Hero = () => {
+  return (
+    <section
+      id="hero"
+      className={`${inter.className} antialiased py-20 container max-w-5xl
+        flex flex-col items-center text-center gap-5`}
+    >
+      <p className="text-xs md:text-sm font-extralight uppercase">
+        Dynamic Web Magic with Next.js
+      </p>
+      <h1 className="text-4xl md:text-6xl font-bold">
+        Developing Seamless User Experiences in Web{' '}
+        <span className="text-secondary">Front-End Development</span>
+      </h1>
+      <h2 className="text-lg md:text-xl">
+        Hi! I’m <span className="text-secondary">Daniel Batista</span>, an
+        experient Front-End Developer based in Portugal
+      </h2>
+    </section>
+  )
+}
