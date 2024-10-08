7147eae0e379bbf3555ebd51e163f374d909d52d
Daniel Batista <dani_batista20@hotmail.com>
2024-10-08 15:29:21 +0100

    add: hero - GridDotBackground

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import { Hero } from '@/components/hero'
 
 export default function Home() {
   return (
-    <div className="text-center">
+    <>
       <Hero />
-    </div>
+    </>
   )
 }
--- components/ui/grind-dot-background.tsx
+++ components/ui/grind-dot-background.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-
 export const GridDotBackground = ({
   children,
 }: Readonly<{ children: React.ReactNode }>) => {
   return (
-    <div className="rounded-4xl w-full dark:bg-black bg-background dark:bg-grid-white/[0.10] bg-grid-black/[0.10] relative flex items-center justify-center">
+    <div className="rounded-4xl w-full dark:bg-transparent bg-background dark:bg-grid-white/[0.10] bg-grid-black/[0.10] relative flex items-center justify-center">
       {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-background bg-background [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       {children}
