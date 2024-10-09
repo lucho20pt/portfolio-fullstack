8098d023ded4b16a3eac3631f9424a5b89a54025
Daniel Batista <dani_batista20@hotmail.com>
2024-10-09 16:02:52 +0100

    add: Highlights - reusable component

---
+++ components/about/highlights.tsx
@@ -0,0 +1,26 @@
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+interface HighlightsProps {
+  title: JSX.Element | string
+  description: (JSX.Element | string)[]
+}
+
+export const Highlights = ({ title, description }: HighlightsProps) => {
+  return (
+    <article
+      className={`${inter.className} antialiased container max-w-5xl
+      flex flex-col text-left gap-4 font-light`}
+    >
+      <h2 className="text-4xl md:text-6xl font-bold my-8">
+        {title}
+      </h2>
+      {description.map((copy, i) => (
+        <p key={i} className="text-md md:text-lg">
+          {copy}
+        </p>
+      ))}
+    </article>
+  )
+}
