d3544255e4fa42c8f4c9c719dea5b3499b9766a2
Daniel Batista <dani_batista20@hotmail.com>
2024-10-09 19:32:06 +0100

    upd: Projects - interface and types

--- components/projects/index.tsx
+++ components/projects/index.tsx
@@ -1,60 +1,48 @@
 import { BentoGrid, BentoGridItem } from '@/components/ui/bento-grid'
 
-const PROJECTS: Projects[] = [
+const PROJECTS: ProjectsProps[] = [
   {
     title: 'Dynamic Web Experiences',
     subtitle: 'Leveraging HTML5 and JavaScript for Modern Web Solutions',
     image: 'path/to/dynamic-web-experiences.jpg',
-    size: 'large',
   },
   {
     title: 'Responsive Design Mastery',
     subtitle:
       'Creating Mobile-First Websites with HTML5 and CSS3, Sass, Bootstrap, and Tailwind',
     image: 'path/to/responsive-design-mastery.jpg',
-    size: 'small',
   },
   {
     title: 'Content Management Excellence',
     subtitle: 'Utilizing WordPress and PHP for Efficient Working Experience',
     image: 'path/to/content-management-excellence.jpg',
-    size: 'small',
   },
   {
     title: 'Advanced Web Applications',
     subtitle: 'Building Scalable Apps with Next.js, TypeScript, and Strapi',
     image: 'path/to/advanced-web-applications.jpg',
-    size: 'large',
   },
   {
     title: 'Headless CMS Integration',
     subtitle: 'Experience with Strapi and Sanity',
     image: 'path/to/headless-cms-integration.jpg',
-    size: 'small',
   },
   {
     title: 'E-commerce Solutions',
     subtitle:
       'Developing Secure and Efficient Online Stores with HTML5, JavaScript, and PHP',
     image: 'path/to/ecommerce-solutions.jpg',
-    size: 'large',
   },
 ]
 
-export interface Projects {
+export interface ProjectsProps {
   title: string
   subtitle?: string
   image?: string
-  size?: 'small' | 'large'
 }
 
-interface ProjectsProps {
-  projects: Projects[]
-}
-
-export const Projects = ({
-  projects = PROJECTS,
-}: ProjectsProps): JSX.Element => {
+export const Projects = (): JSX.Element => {
+  const projects = PROJECTS
   const getClassName = (index: number): string => {
     const groupIndex = Math.floor(index / 8)
     const positionInGroup = index % 4
