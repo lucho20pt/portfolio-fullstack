a900094508bd0db713c81d6163fac48ea117a365
Daniel Batista <dani_batista20@hotmail.com>
2024-10-16 09:00:37 +0100

    upd: heroSchema - return title, subtitle

--- sanity/schemaTypes/components/hero.schema.ts
+++ sanity/schemaTypes/components/hero.schema.ts
@@ -1,10 +1,10 @@
-//hero.schema.ts
 export const hero = {
   name: 'hero',
   title: 'Hero',
   type: 'object',
   options: {
     collapsible: true,
+    editModal: 'fullscreen',
   },
   fields: [
     {
@@ -13,8 +13,8 @@ export const hero = {
       type: 'string',
     },
     {
-      name: 'title',
-      title: 'Title',
+      name: 'heading',
+      title: 'Heading',
       type: 'array',
       of: [{type: 'block'}],
     },
@@ -26,4 +26,16 @@ export const hero = {
     },
     // Add more fields as needed
   ],
+  preview: {
+    select: {
+      subtitle: 'heading.0.children.0.text',
+    },
+    prepare: (value: Record<string, any>) => {
+      const {subtitle} = value
+      return {
+        title: hero.title,
+        subtitle: subtitle || '',
+      }
+    },
+  },
 }
