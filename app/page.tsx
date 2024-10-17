c639ed7fe34045befc91560ff585058b7e426053
Daniel Batista <dani_batista20@hotmail.com>
2024-10-17 18:53:58 +0100

    upd: Hero - HeroProps, getHero(Homepage) | Home - fetch heroData

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,33 @@
-import { Hero } from '@/components/hero'
+'use client'
+import { useEffect, useState } from 'react'
+
+import { Hero, HeroProps } from '@/components/hero'
 import { TechSkills } from '@/components/tech-skills'
 import { About } from '@/components/about'
 import { Projects } from '@/components/projects'
 
+import { getHero } from './actions'
+
 export default function Home() {
+  const [heroData, setHeroData] = useState<HeroProps | null>(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const hero = await getHero('HomePage')
+
+        setHeroData(hero as HeroProps | null)
+      } catch (error) {
+        console.error(error) // Log the error
+      }
+    }
+    fetchData()
+  }, [])
+
   return (
     <>
-      <Hero />
+      {heroData ? <Hero heroData={heroData} /> : <p>{'loading...'}</p>}
+
       <div className="container flex flex-col items-center mx-auto gap-20">
         <TechSkills />
         <About />
--- components/hero.tsx
+++ components/hero.tsx
@@ -5,25 +5,40 @@ import { ButtonPrimary } from '@/components/ui/tailwindcss-buttons'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const Hero = () => {
+// HeroProps
+export interface HeroProps {
+  intro?: string
+  heading: { type: 'block'; children: { text: string }[] }[]
+  description?: { type: 'block'; children: { text: string }[] }[]
+}
+
+export const Hero = ({ heroData }: { heroData: HeroProps }): JSX.Element => {
+  const { intro, heading, description } = heroData
   return (
     <GridDotBackground>
       <section
         className={`${inter.className} antialiased py-24 container max-w-5xl
         flex flex-col items-center text-center gap-6`}
       >
-        <p className="text-xs md:text-sm font-extralight uppercase">
-          Dynamic Web Magic with Next.js
-        </p>
-        <h1 className="text-4xl md:text-6xl font-bold">
-          Developing Seamless User Experiences in Web{' '}
-          <span className="text-secondary">Front-End Development</span>
-        </h1>
-        <h2 className="text-lg md:text-xl">
-          Hi! I’m <span className="text-secondary">Daniel Batista</span>, an
-          experient Front-End Developer based in Portugal
-        </h2>
-        <ButtonPrimary className='text-xl' href="#about">Read More</ButtonPrimary>
+        <p className="text-xs md:text-sm font-extralight uppercase">{intro}</p>
+        {heading.map((headingItem, index) => (
+          <h1 key={index} className="text-4xl md:text-6xl font-bold">
+            {headingItem.children.map((child, childIndex) => (
+              <span key={childIndex}>{child.text}</span>
+            ))}
+          </h1>
+        ))}
+        {description &&
+          description.map((descriptionItem, index) => (
+            <h2 key={index} className="text-lg md:text-xl">
+              {descriptionItem.children.map((child, childIndex) => (
+                <span key={childIndex}>{child.text}</span>
+              ))}
+            </h2>
+          ))}
+        <ButtonPrimary className="text-xl" href="#about">
+          Read More
+        </ButtonPrimary>
       </section>
     </GridDotBackground>
   )
