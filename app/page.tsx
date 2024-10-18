1ab64d6f3dc6da40d78fffbc0b93b787618c81cc
Daniel Batista <dani_batista20@hotmail.com>
2024-10-18 17:16:20 +0100

    add: TechSkills, AnimatedTooltip, Home - implement dynamic data fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,32 @@
 // page.ts
 
 import { Hero, HeroProps } from '@/components/hero'
-import { TechSkills } from '@/components/tech-skills'
+import { TechSkills, TechSkillsProps } from '@/components/tech-skills'
 import { About } from '@/components/about'
 import { Projects } from '@/components/projects'
-import { getHero } from './actions'
+import { getHero, getTechSkills } from './actions'
 
 export const revalidate = 60
 
 export default async function Home() {
   const heroData: HeroProps | null = await getHero('HomePage') // allow for null
-  console.log("home:", heroData)
+  // console.log("heroData:", heroData)
+  const techSkillData: TechSkillsProps | null = await getTechSkills('HomePage') // allow for null
+  // console.log("techSkillData:", techSkillData)
 
   return (
     <>
       {heroData ? <Hero heroData={heroData} /> : <p>{'loading...'}</p>}
+
       <div className="container flex flex-col items-center mx-auto gap-20">
-        <TechSkills />
+        {techSkillData ? (
+          <TechSkills techSkillData={techSkillData} />
+        ) : (
+          <p>{'loading...'}</p>
+        )}
+
         <About />
+
         <Projects />
       </div>
     </>
--- components/ui/animated-tooltip.tsx
+++ components/ui/animated-tooltip.tsx
@@ -8,12 +8,15 @@ import {
   useMotionValue,
   useSpring,
 } from 'framer-motion'
+import { urlFor } from '@/app/utils/sanityImage'
 
 export interface Item {
+  _key: string
   id: number
-  name: string
+  skill: string
   designation: string
   image: string
+  imageUrl: string
 }
 
 interface AnimatedTooltipProps {
@@ -42,60 +45,63 @@ export const AnimatedTooltip = ({
   }
 
   return (
-    <ul className="grid gap-10
-    grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-7">
-      {items.map((item) => (
-        <li
-          className="relative flex cursor-pointer rounded-xl p-6 items-center justify-center h-full 
+    <ul
+      className="grid gap-10
+    grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-7"
+    >
+      {items &&
+        items.map((item, index) => (
+          <li
+            key={item._key}
+            className="relative flex cursor-pointer rounded-xl p-6 items-center justify-center h-full 
            dark:bg-gradient-to-b from-cyan-700 to-transparent shadow-indigo-300
            hover:drop-shadow-xs dark:hover:shadow-foreground hover:bg-indigo-500/10 shadow"
-          key={item.name}
-          onMouseEnter={() => setHoveredIndex(item.id)}
-          onMouseLeave={() => setHoveredIndex(null)}
-        >
-          <AnimatePresence mode="popLayout">
-            {hoveredIndex === item.id && (
-              <motion.div
-                initial={{ opacity: 0, y: 20, scale: 0.6 }}
-                animate={{
-                  opacity: 1,
-                  y: 0,
-                  scale: 1,
-                  transition: {
-                    type: 'spring',
-                    stiffness: 260,
-                    damping: 10,
-                  },
-                }}
-                exit={{ opacity: 0, y: 20, scale: 0.6 }}
-                style={{
-                  translateX: translateX,
-                  rotate: rotate,
-                  whiteSpace: 'nowrap',
-                }}
-                className="absolute -top-16 translate-x-1/2 flex text-xs flex-col items-center justify-center rounded-md z-50 px-4 py-2 
+            onMouseEnter={() => setHoveredIndex(index)}
+            onMouseLeave={() => setHoveredIndex(null)}
+          >
+            <AnimatePresence mode="popLayout">
+              {hoveredIndex === index && (
+                <motion.div
+                  initial={{ opacity: 0, y: 20, scale: 0.6 }}
+                  animate={{
+                    opacity: 1,
+                    y: 0,
+                    scale: 1,
+                    transition: {
+                      type: 'spring',
+                      stiffness: 260,
+                      damping: 10,
+                    },
+                  }}
+                  exit={{ opacity: 0, y: 20, scale: 0.6 }}
+                  style={{
+                    translateX: translateX,
+                    rotate: rotate,
+                    whiteSpace: 'nowrap',
+                  }}
+                  className="absolute -top-16 translate-x-1/2 flex text-xs flex-col items-center justify-center rounded-md z-50 px-4 py-2 
                 bg-indigo-500/80 dark:bg-indigo-500/50
                 dark:border-2 dark:border-secondary"
-              >
-                {/* <div className="absolute inset-x-10 z-30 w-[20%] -bottom-px bg-gradient-to-r from-transparent via-secondary to-transparent h-px " />
+                >
+                  {/* <div className="absolute inset-x-10 z-30 w-[20%] -bottom-px bg-gradient-to-r from-transparent via-secondary to-transparent h-px " />
                 <div className="absolute left-10 w-[40%] z-30 -bottom-px bg-gradient-to-r from-transparent via-secondary to-transparent h-px " /> */}
-                <div className="text-white relative z-30 text-lg">
-                  <>{item.name}</>
-                </div>
-                <div className="text-white text-sm">{item.designation}</div>
-              </motion.div>
-            )}
-          </AnimatePresence>
-          <Image
-            onMouseMove={handleMouseMove}
-            height={100}
-            width={100}
-            src={item.image}
-            alt={item.name}
-            className="object-cover !m-0 !p-0 object-top relative transition duration-500"
-          />
-        </li>
-      ))}
+                  <div className="text-white relative z-30 text-lg">
+                    <>{item.skill}</>
+                  </div>
+                  <div className="text-white text-sm">{item.designation}</div>
+                </motion.div>
+              )}
+            </AnimatePresence>
+            <Image
+              onMouseMove={handleMouseMove}
+              height={100}
+              width={100}
+              alt={item.skill}
+              className="object-cover !m-0 !p-0 object-top relative transition duration-500"
+              src={urlFor(item.image).width(200).url()}
+            />
+          </li>
+        ))}
     </ul>
   )
 }
