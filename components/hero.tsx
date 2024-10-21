9d010567fb708f457678dae7a3e55aff7e74d0ad
Daniel Batista <dani_batista20@hotmail.com>
2024-10-21 16:45:43 +0100

    upd: hero - paddings & gaps

--- components/hero.tsx
+++ components/hero.tsx
@@ -31,7 +31,7 @@ export const Hero = ({
       >
         <p className="text-xs md:text-sm font-extralight uppercase">{intro}</p>
         {heading.map((headingItem, index) => (
-          <h1 key={index} className="text-2xl sm:text-5xl lg:text-6xl font-bold">
+          <h1 key={index} className="text-3xl sm:text-5xl lg:text-6xl font-bold">
             {headingItem.children.map((child, childIndex) => (
               <span
                 key={childIndex}
