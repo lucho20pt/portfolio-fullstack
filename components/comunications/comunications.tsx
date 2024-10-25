625cd79f12dcb0cc605a1111f9cd25a4898eb697
Daniel Batista <dani_batista20@hotmail.com>
2024-10-25 20:35:35 +0100

    upd: home - Comunications

--- components/comunications/comunications.tsx
+++ components/comunications/comunications.tsx
@@ -5,14 +5,13 @@ export const Comunications = () => {
   return (
     <section className="flex flex-col items-center justify-center text-center">
       <header className="container">
-        <h2 className="text-xl md:text-3xl font-extralight uppercase md:px-10 text-slate-400">
+        <h2 className="text-xl md:text-3xl font-extralight uppercase md:px-10 text-slate-400 max-w-3xl">
           I’m very flexible with time zone communications
         </h2>
       </header>
       <div
         className="overflow-hidden flex flex-row items-start justify-center
-        h-[200px] shadow-secondary shadow-[0px_8px_8px_-8px]
-      "
+        h-[200px] shadow-secondary shadow-[0px_8px_8px_-8px]"
       >
         <MyGlobe className="w-[400px] h-[400px]" />
       </div>
