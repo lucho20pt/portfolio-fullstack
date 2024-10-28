e4e21a5dc1083e80b637248155f2e4ebae9c346e
Daniel Batista <dani_batista20@hotmail.com>
2024-10-28 23:42:11 +0000

    add: actions - getServices()

--- app/actions/index.ts
+++ app/actions/index.ts
@@ -4,30 +4,16 @@
 import { client } from '@/app/utils/client'
 import { HeroProps } from '@/components/hero'
 import { TechSkillsProps } from '@/components/tech-skills'
-import type {
-  Hero,
-  Page,
-  TechSkillList,
-  //   TechSkill,
-  //   Slug,
-  //   SanityImageAsset,
-  //   SanityImageCrop,
-  //   SanityImageHotspot,
-  //   SanityImageMetadata,
-  //   SanityImagePalette,
-  //   SanityImagePaletteSwatch,
-  //   SanityImageDimensions,
-  //   SanityFileAsset,
-  //   Geopoint,
-} from '@/sanity.types'
+import { ServicesProps } from '@/components/services/services'
+import type { Hero, Page, TechSkillList, Services } from '@/sanity.types'
 
 // PageProps interface
 export type PageProps = Page
 
 // Get all pages
 export async function getAllPages(): Promise<Page[]> {
-    const data = await client.fetch<Page[]>(`*[_type == "page"] `)
-    return data
+  const data = await client.fetch<Page[]>(`*[_type == "page"] `)
+  return data
 }
 
 // Get HERO component data
@@ -53,7 +39,6 @@ export async function getHero(page: string): Promise<HeroProps | null> {
   }
 }
 
-//"imageUrl": asset->url
 // Get TECH_SKILLS component data
 export async function getTechSkills(
   page: string
@@ -93,6 +78,48 @@ export async function getTechSkills(
     return null
   } catch (error) {
     console.error('Error fetching tech skills data:', error)
+    return null
+  }
+}
+
+// Get SERVICES component data
+export async function getServices(page: string): Promise<ServicesProps | null> {
+  try {
+    const data = await client.fetch<Page[]>(`
+      *[_type == "page" && title.current == "${page}"] 
+        {
+          components[] {
+            _key,
+            _type,
+            intro,
+            service[] {
+              _key,
+              _type,
+              title,
+              description,
+              image {
+                asset -> {
+                  url
+                }
+              }
+            }
+          }
+        }
+    `)
+
+    if (data.length > 0) {
+      const servicesComponent = data[0].components?.find(
+        (component): component is { _key: string } & Services =>
+          component._type === 'services'
+      )
+      if (servicesComponent) {
+        return Object(servicesComponent) // Adjust to return skills
+      }
+    }
+
+    return null
+  } catch (error) {
+    console.error('Error fetching services data:', error)
     return null
   }
 }
