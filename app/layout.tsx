afa5efddeedc6cfc898b4beff5afcab17e4f4356
Daniel Batista <dani_batista20@hotmail.com>
2024-10-29 22:03:01 +0000

    fix: Layout - overflow

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="dark">
         <Header />
-        <main className="flex flex-col items-center mx-auto gap-12 pb-20 max-w-7xl">
+        <main className="flex flex-col items-center mx-auto gap-12 pb-20 max-w-7xl overflow-hidden">
           {children}
         </main>
         <Footer />
