b751e1dcb79b667ca73b9908f0b90014128b2a5c
Daniel Batista <dani_batista20@hotmail.com>
2024-10-30 17:23:29 +0000

    upd: Footer - redesign semantic & styles

--- components/footer.tsx
+++ components/footer.tsx
@@ -20,71 +20,91 @@ export const Footer = () => {
     }
   }
   return (
-    <div className="border-t-2 border-secondary py-10 pb-0">
-      <div className="container flex flex-row items-center justify-between text-slate-400 text-sm">
-        <Link href="/" className="hover:text-slate-100">
-          <Image
-            src="/logo2.svg"
-            alt="Daniel Batista"
-            width={0}
-            height={0}
-            sizes="100vw"
-            className="w-full h-auto max-w-[50px] md:max-w-[70px] opacity-80"
-          />
-        </Link>
+    <footer className='py-5'>
+      <div className="container flex flex-col text-slate-400 text-sm">
+        {/* top */}
+        <div className="flex flex-row items-start justify-between py-5
+        border-t border-b border-secondary">
+          {/* logo  */}
+          <Link href="/" className="hover:text-slate-100 self-center">
+            <Image
+              src="/logo2.svg"
+              alt="Daniel Batista"
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="w-full h-auto max-w-[50px] md:max-w-[70px] opacity-80"
+            />
+          </Link>
 
-        {/* navs */}
-        <div className="flex flex-row gap-12 sm:gap-12 w-[50%] sm:w-auto">
-          <nav>
-            <p className="mb-4">
-              <b>Social</b>
-            </p>
-            <ul className="flex flex-col gap-5 text-3xl hover:[&_*]:text-indigo-500">
-              <li>
-                <Link
-                  href="https://www.linkedin.com/in/daniel-batista-0609a430"
-                  target="_blank"
-                >
-                  <FaLinkedin title="Visit my Linkedin" />
-                </Link>
-              </li>
-              <li>
-                <button
-                  onClick={handleCopy}
-                  className={`${showNotification ? 'text-indigo-500 hover:text-indigo-500' : ''}`}
-                >
-                  <FaLink title="Copy Shared Link" />
-                </button>
-              </li>
-            </ul>
-          </nav>
-          
-          <nav>
-            <p className="mb-4">
-              <b>Sitemap</b>
-            </p>
-            <ul className="flex flex-col [&_*]:mb-2 hover:[&_*]:text-indigo-500">
-              <li>
-                <Link href="/">Home</Link>
-              </li>
-              <li>
-                <Link href="/">About</Link>
-              </li>
-              <li>
-                <Link href="/">Projects</Link>
-              </li>
-              <li>
-                <Link href="/">Contact</Link>
-              </li>
-              <li>
-                <Link href="/">Privacy</Link>
-              </li>
-            </ul>
-          </nav>
+          <div className="flex flex-row items-start justify-around gap-16">
+            {/* social  */}
+            <nav>
+              <p className="mb-4">
+                <b>Social</b>
+              </p>
+              <ul className="flex flex-col gap-5 text-3xl hover:[&_*]:text-indigo-500">
+                <li>
+                  <Link
+                    href="https://www.linkedin.com/in/daniel-batista-0609a430"
+                    target="_blank"
+                  >
+                    <FaLinkedin title="Visit my Linkedin" />
+                  </Link>
+                </li>
+                <li>
+                  <button
+                    onClick={handleCopy}
+                    className={`${showNotification ? 'text-indigo-500 hover:text-indigo-500' : ''}`}
+                  >
+                    <FaLink title="Copy Shared Link" />
+                  </button>
+                </li>
+              </ul>
+            </nav>
+
+            {/* sitemap  */}
+            <nav>
+              <p className="mb-4">
+                <b>Sitemap</b>
+              </p>
+              <ul className="flex flex-col [&_*]:mb-2 hover:[&_*]:text-indigo-500">
+                <li>
+                  <Link href="/">Home</Link>
+                </li>
+                <li>
+                  <Link href="/about">About</Link>
+                </li>
+                <li>
+                  <Link href="/projects">Projects</Link>
+                </li>
+                <li>
+                  <Link href="/contact">Contact</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+
+        {/* bottom */}
+        <div className="flex flex-row items-center justify-between py-5">
+          <p className="text-center text-slate-400 text-xs">
+            @Copyright 2024 Daniel Batista. All rights reserved.
+          </p>
+
+          <ul
+            className="flex flex-row items-center justify-center gap-4 
+            hover:[&_*]:text-indigo-500"
+          >
+            <li>
+              <Link href="/terms">Terms</Link>
+            </li>
+            <li>
+              <Link href="/privacy">Privacy</Link>
+            </li>
+          </ul>
         </div>
       </div>
-
-      <p className="text-center text-slate-400 text-xs py-4">@Copyright 2024</p>
 
       {/* Notification */}
       {showNotification && (
@@ -106,6 +126,6 @@ export const Footer = () => {
           Copied to the clipboard!
         </div>
       )}
-    </div>
+    </footer>
   )
 }
