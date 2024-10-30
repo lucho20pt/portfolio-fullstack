67a4828684a06b184f1a7b19c27abaee7d7a961a
Daniel Batista <dani_batista20@hotmail.com>
2024-10-30 02:54:16 +0000

    fix: metadata - how to render metaData

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 // home.ts
-import Head from 'next/head'
+import { Metadata } from 'next'
 import { Hero, HeroProps } from '@/components/hero'
 import { TechSkills, TechSkillsProps } from '@/components/tech-skills'
 import { getHero, getTechSkills, getServices, getMetadata } from './actions'
@@ -7,21 +7,21 @@ import { Comunications } from '@/components/comunications/comunications'
 import { Services, ServicesProps } from '@/components/services/services'
 
 export const revalidate = 60
+const page = 'HomePage'
+const metaData = await getMetadata(page)
+
+export const metadata: Metadata = {
+  title: metaData?.title,
+  description: metaData?.description,
+}
 
 export default async function Home() {
-  const page = 'HomePage'
   const heroData: HeroProps | null = await getHero(page) // allow for null
   const techSkillData: TechSkillsProps | null = await getTechSkills(page) // allow for null
   const servicesData: ServicesProps | null = await getServices(page) // allow for null
-  const metaData = await getMetadata(page)
 
   return (
     <>
-      <Head>
-        <title>{metaData?.title}</title>
-        <meta name="description" content={metaData?.description} />
-      </Head>
-
       {heroData ? <Hero heroData={heroData} /> : <p>{'loading...'}</p>}
 
       <div className="container flex flex-col items-center mx-auto gap-28 max-w-full">
