59eb57debc10aee893e6a471134e964cbb5fd9af
Daniel Batista <dani_batista20@hotmail.com>
2024-11-08 18:15:44 +0000

    upd: ContactForm - handleSubmit()

--- components/contact/contact-form.tsx
+++ components/contact/contact-form.tsx
@@ -2,14 +2,39 @@
 
 import React, { useState } from 'react'
 import { ButtonPrimary } from '@/components/ui/tailwindcss-buttons'
+import { sendEmail } from '@/app/actions'
 
 export const ContactForm = () => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [message, setMessage] = useState('')
+  const [error, setError] = useState('')
+  const [success, setSuccess] = useState('')
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
+    if (!name || !email || !message) {
+      setError('Please fill out all fields.')
+      return
+    }
+    setError('') // Clear previous error message
+
+    try {
+      const response = await sendEmail(name, email, message)
+      console.log('response', response)
+
+      if (response.success) {
+        setSuccess('Thank you! Your message has been sent.')
+        setName('')
+        setEmail('')
+        setMessage('')
+      } else {
+        setError(response.message)
+      }
+    } catch (error) {
+      setError('Error sending message. Please try again later.')
+      return error
+    }
   }
 
   return (
@@ -19,8 +44,7 @@ export const ContactForm = () => {
           Contact Me
         </h2>
       </header>
-      {/* {error && <div className="text-red-500 mb-4">{error}</div>}
-      {success && <div className="text-green-500 mb-4">{success}</div>} */}
+
       <form onSubmit={handleSubmit} className="flex flex-col gap-12">
         <div className="mb-4">
           <label className="block text-foreground text-lg font-extrabold dark:text-slate-400">
@@ -39,7 +63,7 @@ export const ContactForm = () => {
           </label>
           <input
             type="email"
-            className="w-full p-2 border-gray-300 mt-1 bg-transparent border-b-2 border-secondary focus:border-indigo-500 outline-none"
+            className="w-full p-2 border-gray-300 mt-1 bg-transparent autofill:!bg-indigo-500 border-b-2 border-secondary focus:border-indigo-500 outline-none"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
@@ -55,6 +79,8 @@ export const ContactForm = () => {
           />
         </div>
         <div className="text-center">
+          {error && <div className="text-red-500 mb-4">{error}</div>}
+          {success && <div className="text-green-500 mb-4">{success}</div>}
           <ButtonPrimary
             type="submit"
             className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
