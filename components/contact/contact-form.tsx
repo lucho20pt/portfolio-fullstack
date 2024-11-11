572c96ab19d0b45a51f9d47d7ad30300678aa603
Daniel Batista <dani_batista20@hotmail.com>
2024-11-11 18:07:19 +0000

    upd: ContactForm - with FormFieldFormFieldText, FormFieldTextArea

--- components/contact/contact-form.tsx
+++ components/contact/contact-form.tsx
@@ -3,6 +3,11 @@
 import React, { useState } from 'react'
 import { ButtonPrimary } from '@/components/ui/tailwindcss-buttons'
 import { sendEmail } from '@/app/actions'
+import {
+  FormFieldText,
+  FormFieldTextArea,
+} from '@/components/contact/form-field'
+import { Notification } from '@/components/notification'
 
 export const ContactForm = () => {
   const [subject, setSubject] = useState('')
@@ -11,12 +16,19 @@ export const ContactForm = () => {
   const [message, setMessage] = useState('')
   const [error, setError] = useState('')
   const [success, setSuccess] = useState('')
+  const [showNotification, setShowNotification] = useState(false)
+  const [notificationType, setNotificationType] = useState<
+    'success' | 'error'
+  >()
   const [consent, setConsent] = useState(false)
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
     if (!subject || !name || !email || !message || !consent) {
       setError('Please fill out all fields and agree to the consent.')
+      setNotificationType('error')
+      setShowNotification(true)
+      setTimeout(() => setShowNotification(false), 3000)
       return
     }
     setError('') // Clear previous error message
@@ -32,11 +44,20 @@ export const ContactForm = () => {
         setEmail('')
         setMessage('')
         setConsent(false)
+        setNotificationType('success')
+        setShowNotification(true)
+        setTimeout(() => setShowNotification(false), 3000)
       } else {
         setError(response.message)
+        setNotificationType('error')
+        setShowNotification(true)
+        setTimeout(() => setShowNotification(false), 3000)
       }
     } catch (error) {
       setError('Error sending message. Please try again later.')
+      setNotificationType('error')
+      setShowNotification(true)
+      setTimeout(() => setShowNotification(false), 3000)
       return error
     }
   }
@@ -50,57 +71,39 @@ export const ContactForm = () => {
       </header>
 
       <form onSubmit={handleSubmit} className="flex flex-col gap-12">
-        <div className="mb-4">
-          <label htmlFor="subject" className="block text-foreground text-lg font-extrabold dark:text-slate-400">
-            Subject*
-          </label>
-          <input
-            type="text"
-            id="subject"
-            name="subject"
-            className="w-full p-2 border-gray-300 mt-1 bg-transparent border-b-2 border-secondary focus:border-indigo-500 outline-none"
-            value={subject}
-            onChange={(e) => setSubject(e.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="name" className="block text-foreground text-lg font-extrabold dark:text-slate-400">
-            Name*
-          </label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            className="w-full p-2 border-gray-300 mt-1 bg-transparent border-b-2 border-secondary focus:border-indigo-500 outline-none"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="email" className="block text-foreground text-lg font-extrabold dark:text-slate-400">
-            Email*
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            className="w-full p-2 border-gray-300 mt-1 bg-transparent autofill:!bg-indigo-500 border-b-2 border-secondary focus:border-indigo-500 outline-none"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="message" className="block text-foreground text-lg font-extrabold dark:text-slate-400">
-            Message*
-          </label>
-          <textarea
-            id="message"
-            name="message"
-            className="w-full p-2 border-gray-300 mt-1 bg-transparent border-b-2 border-secondary focus:border-indigo-500 outline-none"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          />
-        </div>
+        <FormFieldText
+          label="Subject*"
+          id="subject"
+          name="subject"
+          type="text"
+          value={subject}
+          onChange={(e) => setSubject(e.target.value)}
+        />
+        <FormFieldText
+          label="Name*"
+          id="name"
+          name="name"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <FormFieldText
+          label="Email*"
+          id="email"
+          name="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <FormFieldTextArea
+          label="Message*"
+          id="message"
+          name="message"
+          type="text"
+          rows={3}
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
         <div className="mb-4 flex items-center">
           <input
             type="checkbox"
@@ -108,9 +111,12 @@ export const ContactForm = () => {
             name="consent"
             checked={consent}
             onChange={(e) => setConsent(e.target.checked)}
-            className='cursor-pointer'
+            className="cursor-pointer"
           />
-          <label htmlFor="consent" className="ml-2 text-foreground text-sm dark:text-slate-400 cursor-pointer">
+          <label
+            htmlFor="consent"
+            className="ml-2 text-foreground text-sm dark:text-slate-400 cursor-pointer"
+          >
             I consent that I have read the terms & privacy policy.
           </label>
         </div>
@@ -123,46 +129,11 @@ export const ContactForm = () => {
           </ButtonPrimary>
         </div>
       </form>
-      <div>
-        {error && (
-          <div
-            style={{
-              position: 'fixed',
-              bottom: '20px',
-              left: '50%',
-              transform: 'translateX(-50%)',
-              backgroundColor: '#333',
-              color: 'text-red-500',
-              padding: '10px 20px',
-              borderRadius: '4px',
-              boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-              zIndex: 1000,
-              transition: 'opacity 0.3s ease-in-out',
-            }}
-          >
-            {error}
-          </div>
-        )}
-        {success && (
-          <div
-            style={{
-              position: 'fixed',
-              bottom: '20px',
-              left: '50%',
-              transform: 'translateX(-50%)',
-              backgroundColor: '#333',
-              color: 'text-green-500',
-              padding: '10px 20px',
-              borderRadius: '4px',
-              boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-              zIndex: 1000,
-              transition: 'opacity 0.3s ease-in-out',
-            }}
-          >
-            {success}
-          </div>
-        )}
-      </div>
+      {showNotification && (
+        <Notification message={error || success} type={notificationType} />
+      )}
     </section>
   )
 }
+
+export default ContactForm
