fc2c9a25b76a9efd8cc57e3c3edf257462c29622
Daniel Batista <dani_batista20@hotmail.com>
2024-11-13 18:31:16 +0000

    upd: HeadingPrimary - paddings

--- components/ui/headings.tsx
+++ components/ui/headings.tsx
@@ -5,7 +5,7 @@ export const HeadingPrimary = ({
   className,
 }: Readonly<{ children: React.ReactNode; className?: string }>) => {
   return (
-    <h2 className={`${className} w-full text-2xl md:text-3xl font-extralight uppercase p-10 text-slate-400 max-w-3xl text-center mx-auto`}>
+    <h2 className={`${className} w-full text-2xl md:text-3xl font-extralight uppercase py-10 text-slate-400 max-w-3xl text-center mx-auto`}>
       {children}
     </h2>
   )
