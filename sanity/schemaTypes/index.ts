acdc50402003933c1f8985088755d39b3a774369
Daniel Batista <dani_batista20@hotmail.com>
2024-11-14 01:10:25 +0000

    upd: tech_skill to tech-skill

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,6 +1,6 @@
 import {page} from './page.schema'
 import {hero} from './components/hero.schema'
-import {techSkill, techSkillList} from './components/tech_skill.schema'
+import {techSkill, techSkillList} from './components/tech-skill.schema'
 import {services, service} from './components/services.schema'
 import {article} from './components/article.schema'
 
